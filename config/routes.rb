f0afa155ea48967ff6e161e69071a608bb5a95ca
NguyenHungHust256 <hungpronguyen256@gmail.com>
2019-07-18 17:24:36 +0700

    man hinh chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
-    root "static_pages#home"
     devise_for :users
+    root "static_pages#home"
+    get "/search-users", to: "search_users#search_user_by_email"
+    get "/get-user", to: "search_users#get_all_users"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: "Thái Thị Tiến",
+             username: "thaitien",
+             email: "thaithitien1997@gmail.com",
+             password: "12345678",
+             gender: 2,
+             birthday: "25/06/1997",
+             password_confirmation: "12345678")
+User.create!(name: "Nguyen Ba Hung",
+             username: "bahung",
+             email: "hungpronguyen256@gmail.com",
+             password: "12345678",
+             gender: 1,
+             birthday: "25/06/1997",
+             password_confirmation: "12345678",
+             admin: true)
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               username: "bahung-#{n}",
+               email: email,
+               password: password,
+               gender: 1,
+               birthday: "25/06/1997",
+               password_confirmation: password)
+end
